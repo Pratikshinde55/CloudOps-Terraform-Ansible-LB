32b96210de6e1b7e300dcd67c26add1e1b9e84fe
Pratik Shinde <145910708+Pratikshinde55@users.noreply.github.com>
2025-01-10 16:03:17 +0530

    Update ansible-setup.sh

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 
-# Update the system
 echo "Updating the system..."
 sudo yum update -y
 
-# Install EPEL repository
 echo "Installing Amazon Linux Extras and EPEL..."
 sudo amazon-linux-extras install epel -y
-
-# Install Ansible 
+ 
 echo "Installing Ansible..."
 sudo yum install ansible -y
 
 # Ansible Setup done message show 
 echo "Ansible installation complete!"
 
-#install git
 echo "Hey git is installing"
 sudo yum install git -y
 
 # Creade DIR under psadmin user
-mkdir /home/psadmin/Pratik-lB-git
+mkdir /home/psadmin/Pratik-Git-lb
 
-#Change to DIR
-cd /home/psadmin/Pratik-lB-git
+cd /home/psadmin/Pratik-Git-lb
 
 # Git clone cmd for clone my Ansible LB REPO
 git clone https://github.com/Pratikshinde55/Ansible-loadBalancer-webserver-configuration.git
-
-
-
